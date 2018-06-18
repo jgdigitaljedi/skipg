fc5c14fd37381e800c49ee27df1f82a1fd0b8c0a
Joey Gauthier <jgauthier@r4.co>
2018-06-18 07:55:00 -0500

    get photos list retrieval working

--- src/app/services/cloudinary.service.ts
+++ src/app/services/cloudinary.service.ts
@@ -7,20 +7,18 @@ import { Photo } from './photo';
 import { Cloudinary } from '@cloudinary/angular-5.x';
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root'
 })
 export class CloudinaryService {
-	constructor(private http: HttpClient, private cloudinary: Cloudinary) {}
+  constructor(private http: HttpClient, private cloudinary: Cloudinary) { }
 
-	getPhotos(): Observable<Photo[]> {
-		// instead of maintaining the list of images, we rely on the 'myphotoalbum' tag
-		// and simply retrieve a list of all images with that tag.
-		let url = this.cloudinary.url('skipg', {
-			format: 'json',
-			type: 'list',
-			version: Math.ceil(new Date().getTime() / 1000)
-		});
-
-		return this.http.get(url).pipe(map((data: any) => data.resources));
-	}
+  getPhotos(): Observable<Photo[]> {
+    let url = this.cloudinary.url('skipg', {
+      format: 'json',
+      type: 'list',
+      version: Math.ceil(new Date().getTime() / 1000)
+    });
+    console.log('url', url);
+    return this.http.get(url).pipe(map((data: any) => data.resources));
+  }
 }
--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+// import { Observable } from 'rxjs/Observable';
+import { CloudinaryService } from '../services/cloudinary.service';
+import { Photo } from '../services/photo';
+
 
 @Component({
   selector: 'app-view',
@@ -6,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view.component.scss']
 })
 export class ViewComponent implements OnInit {
+  private photos;
 
-  constructor() { }
+  constructor(private _cloud: CloudinaryService) { }
 
   ngOnInit() {
+    this._cloud.getPhotos()
+      .subscribe((res) => {
+        this.photos = res;
+        console.log('asdfasdfasdf', res);
+      });
   }
 
 }
