9ee829f5cfc611ecbe27dd8b95f8ad20fd87dd80
Joey Gauthier <digitaljedi@outlook.com>
2018-06-27 20:55:18 -0500

    meta tags, fixed mixed content issue on Android

--- src/app/services/cloudinary.service.ts
+++ src/app/services/cloudinary.service.ts
@@ -7,22 +7,22 @@ import { Photo } from './photo';
 import { Cloudinary } from '@cloudinary/angular-5.x';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class CloudinaryService {
-  constructor(private http: HttpClient, private cloudinary: Cloudinary) { }
+	constructor(private http: HttpClient, private cloudinary: Cloudinary) {}
 
-  getPhotos(): Observable<Photo[]> {
-    let url = this.cloudinary.url('skipg', {
-      format: 'json',
-      type: 'list',
-      version: Math.ceil(new Date().getTime() / 1000)
-    });
-    console.log('url', url);
-    return this.http.get(url).pipe(map((data: any) => data.resources));
-  }
+	getPhotos(): Observable<Photo[]> {
+		let url = this.cloudinary.url('skipg', {
+			format: 'json',
+			type: 'list',
+			version: Math.ceil(new Date().getTime() / 1000)
+		});
+		console.log('url', url);
+		return this.http.get(url).pipe(map((data: any) => data.resources));
+	}
 
-  photoBaseUrl() {
-    return 'http://res.cloudinary.com/skipg-me/image/upload/v1529667001/';
-  }
+	photoBaseUrl() {
+		return 'https://res.cloudinary.com/skipg-me/image/upload/v1529667001/';
+	}
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false
+	production: false
 };
 
 export const gaKey = '';
