09e5b2e4203c56480dd72cfadd332a13b564d6ab
Joey Gauthier <jgauthier@r4.co>
2018-06-28 15:07:55 -0500

    experimenting with slideshow animations

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -1,77 +1,87 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { fadeInAnimation } from '../../styles/animations/animations';
+import { fadeInAnimation, fadeInAnim2, growAnimation } from '../../styles/animations/animations';
 import { Photo } from '../services/photo';
 import { CloudinaryService } from '../services/cloudinary.service';
 import { ISSState } from '../models/slideshow.model';
 
 @Component({
-	selector: 'app-slideshow',
-	templateUrl: './slideshow.component.html',
-	styleUrls: [ './slideshow.component.scss' ],
-	animations: [ fadeInAnimation ],
-	host: { '[@fadeInAnimation]': '' }
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss'],
+  animations: [fadeInAnimation, growAnimation, fadeInAnim2],
+  host: { '[@fadeInAnimation]': '' }
 })
 export class SlideshowComponent implements OnInit, OnDestroy {
-	currentImage: string;
-	allPhotos: Photo[];
-	baseUrl: string;
-	state: ISSState;
-	timer;
-	interval = 5000;
-	isPaused = false;
+  currentImage: string;
+  allPhotos: Photo[];
+  baseUrl: string;
+  state: ISSState;
+  timer;
+  interval = 5000;
+  isPaused = false;
+  animation: string;
+  animationArr = [
+    'fadeIn'
+  ];
 
-	constructor(private _cloud: CloudinaryService) {}
+  constructor(private _cloud: CloudinaryService) { }
 
-	ngOnInit() {
-		this.baseUrl = this._cloud.photoBaseUrl();
-		this._cloud.getPhotos().subscribe((res) => {
-			this.allPhotos = res;
-			this.state = {
-				pLen: res.length,
-				currIndex: 0
-			};
-			this.currentImage = this.baseUrl + this.allPhotos[this.state.currIndex].public_id;
-			console.log('all photos', res);
-			this.startShow();
-		});
-	}
+  ngOnInit() {
+    this.baseUrl = this._cloud.photoBaseUrl();
+    this._cloud.getPhotos().subscribe((res) => {
+      this.allPhotos = res;
+      this.state = {
+        pLen: res.length,
+        currIndex: 0
+      };
+      this.currentImage = this.baseUrl + this.allPhotos[this.state.currIndex].public_id;
+      this.startShow();
+    });
+  }
 
-	ngOnDestroy() {
-		clearInterval(this.timer);
-	}
+  ngOnDestroy() {
+    clearInterval(this.timer);
+  }
 
-	startShow() {
-		this.isPaused = false;
-		this._timer();
-	}
+  startShow() {
+    this.animation = this.animationArr[0];
+    this.isPaused = false;
+    this._timer();
+  }
 
-	pauseShow() {
-		this.isPaused = true;
-		clearInterval(this.timer);
-	}
+  pauseShow() {
+    this.isPaused = true;
+    clearInterval(this.timer);
+  }
 
-	changeSlide(dir) {
-		clearInterval(this.timer);
-		if (dir === 'back') {
-			this.state.currIndex--;
-			// this.state.currIndex = this.state.currIndex - 2;
-		} else {
-			this.state.currIndex++;
-		}
-		this.currentImage = this.baseUrl + this.allPhotos[this.state.currIndex].public_id;
-		this._timer();
-	}
+  changeSlide(dir) {
+    clearInterval(this.timer);
+    if (dir === 'back') {
+      this.state.currIndex--;
+      // this.state.currIndex = this.state.currIndex - 2;
+    } else {
+      this.state.currIndex++;
+    }
+    this.currentImage = this.baseUrl + this.allPhotos[this.state.currIndex].public_id;
+    this._timer();
+  }
 
-	private _timer() {
-		this.timer = setInterval(() => {
-			const nextIndex = this.state.currIndex + 1;
-			if (nextIndex < this.state.pLen) {
-				this.state.currIndex = nextIndex;
-			} else {
-				this.state.currIndex = 0;
-			}
-			console.log('interval', this.state.currIndex);
-			this.currentImage = this.baseUrl + this.allPhotos[this.state.currIndex].public_id;
-		}, this.interval);
-	}
+  private _timer() {
+    this.timer = setInterval(() => {
+      this.handleAnimation();
+      const nextIndex = this.state.currIndex + 1;
+      if (nextIndex < this.state.pLen) {
+        this.state.currIndex = nextIndex;
+      } else {
+        this.state.currIndex = 0;
+      }
+      console.log('interval', this.state.currIndex);
+      this.currentImage = this.baseUrl + this.allPhotos[this.state.currIndex].public_id;
+    }, this.interval);
+  }
+
+  private handleAnimation() {
+    // testing only; will add real logic when concept proven to work
+    this.animation = this.animation === 'fadIn' ? 'grow' : 'fadeIn';
+  }
 }
