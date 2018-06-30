94899244a8257d7f593fbff389b3d4869c4e43fc
Joey Gauthier <digitaljedi@outlook.com>
2018-06-30 09:49:56 -0500

    added music playing, navbar style work, style cleanup

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -4,6 +4,7 @@ import { Photo } from '../services/photo';
 import { CloudinaryService } from '../services/cloudinary.service';
 import { ISSState } from '../models/slideshow.model';
 import * as _shuffle from 'lodash/shuffle';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
 	selector: 'app-slideshow',
@@ -18,7 +19,7 @@ export class SlideshowComponent implements OnInit, OnDestroy {
 	baseUrl: string;
 	state: ISSState;
 	timer;
-	interval = 5000;
+	interval = 6000;
 	isPaused = false;
 	animation: string;
 	musicPaused = true;
@@ -43,10 +44,25 @@ export class SlideshowComponent implements OnInit, OnDestroy {
 		'zoomInUp',
 		'rollIn'
 	];
+	audio;
+	songs = [
+		'../../assets/songs/proudMary.mp3',
+		'../../assets/songs/dancingInTheStreet.mp3',
+		'../../assets/songs/gotYouBabe.mp3'
+	];
+	songIndex = -1;
+	routeSub;
 
-	constructor(private _cloud: CloudinaryService) {}
+	constructor(private _cloud: CloudinaryService, private _route: Router) {}
 
 	ngOnInit() {
+		this.routeSub = this._route.events.subscribe((event) => {
+			if (event instanceof NavigationStart) {
+				this.audio.pause();
+				this.audio = null;
+				clearInterval(this.timer);
+			}
+		});
 		this.baseUrl = this._cloud.photoBaseUrl();
 		this._cloud.getPhotos().subscribe((res) => {
 			this.allPhotos = _shuffle(res);
@@ -60,6 +76,8 @@ export class SlideshowComponent implements OnInit, OnDestroy {
 	}
 
 	ngOnDestroy() {
+		this.routeSub.unsubscribe();
+		this.audio = null;
 		clearInterval(this.timer);
 	}
 
@@ -83,6 +101,41 @@ export class SlideshowComponent implements OnInit, OnDestroy {
 		}
 		this.currentImage = this.baseUrl + this.allPhotos[this.state.currIndex].public_id;
 		this._timer();
+	}
+
+	playPauseMusic(noPause?) {
+		if (!this.audio) {
+			this.audio = new Audio();
+		}
+		if (!noPause) {
+			this.musicPaused = !this.musicPaused;
+		}
+		if (this.musicPaused) {
+			this.audio.pause();
+			this.audio = null;
+		} else {
+			// this.audio.src = this.songs[this.songIndex];
+			// this.audio.load();
+			this._loadAudio();
+			this.audio.play();
+			this.musicPaused = false;
+			this.audio.onended = () => {
+				// this._loadAudio();
+				// this.audio.play();
+				this.playPauseMusic(true);
+			};
+		}
+	}
+
+	private _loadAudio() {
+		this.songIndex++;
+		if (this.songIndex === this.songs.length) {
+			this.songIndex = 0;
+		}
+		this.audio = null;
+		this.audio = new Audio();
+		this.audio.src = this.songs[this.songIndex];
+		this.audio.load();
 	}
 
 	private _timer() {
