db5d1082fdb4d1dcb8852d8e5f4d621813c698b2
Joey Gauthier <digitaljedi@outlook.com>
2020-08-18 19:18:14 -0500

    updated colors for grandmas page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, UrlSegment } from '@angular/router';
-import { environment, gaKey } from '../environments/environment';
-import { GoogleAnalyticsService } from './services/google-analytics.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, UrlSegment } from "@angular/router";
+import { environment, gaKey } from "../environments/environment";
+import { GoogleAnalyticsService } from "./services/google-analytics.service";
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: [ './app.component.scss' ]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-	// activeRoute: BehaviorSubject;
+  // activeRoute: BehaviorSubject;
 
-	constructor(private _activated: ActivatedRoute, private _ga: GoogleAnalyticsService) {}
+  constructor(private _activated: ActivatedRoute, private _ga: GoogleAnalyticsService) {}
 
-	ngOnInit() {
-		if (environment && environment.production) {
-			this._appendGaTrackingCode();
-		}
-		this._activated.url.subscribe((url: UrlSegment[]) => {
-			// console.log('url', url);
-		});
-	}
+  ngOnInit() {
+    if (environment && environment.production) {
+      this._appendGaTrackingCode();
+    }
+    this._activated.url.subscribe((url: UrlSegment[]) => {
+      // console.log('url', url);
+    });
+  }
 
-	private _appendGaTrackingCode() {
-		try {
-			const script = document.createElement('script');
-			script.innerHTML =
-				`
+  private _appendGaTrackingCode() {
+    try {
+      const script = document.createElement("script");
+      script.innerHTML =
+        `
 	      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 	      ga('create', '` +
-				gaKey +
-				`', 'auto');
+        gaKey +
+        `', 'auto');
 	    `;
-			document.head.appendChild(script);
-		} catch (ex) {
-			console.error('Error appending google analytics');
-			console.error(ex);
-		}
-	}
+      document.head.appendChild(script);
+    } catch (ex) {
+      console.error("Error appending google analytics");
+      console.error(ex);
+    }
+  }
 }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Router, RouterEvent } from '@angular/router';
+import { Component, OnInit, Input } from "@angular/core";
+import { Router, RouterEvent } from "@angular/router";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent implements OnInit {
   @Input() activeRoute: string;
+  isShirley: boolean;
 
-  constructor(private _route: Router) { }
+  constructor(private _route: Router) {}
 
   ngOnInit() {
     this._route.events.subscribe((val: RouterEvent) => {
+      console.log("val", val);
       this.activeRoute = val.url;
+      this.isShirley = val.url === "/shirley";
     });
   }
 
